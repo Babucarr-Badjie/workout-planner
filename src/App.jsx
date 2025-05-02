c5f7fdd282240cf5b4cb346da34df92afbfc5a2c
babucarr <babucarr.badjie12@gmail.com>
2025-05-03 01:38:43 +0800

    Hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Layout from "./components/Layout";
 import Grid from "./components/Grid";
+import Hero from "./components/Hero";
 function App() {
   return (
     <>
       <Layout>
         <main>
+          <Hero />
           <Grid />
         </main>
       </Layout>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,33 @@
 export default function Layout(props) {
   const { children } = props;
-  return <>{children}</>;
+
+  const header = (
+    <header>
+      <h1 className="text-gradient">Workout Planner</h1>
+      <p>
+        <strong>The 30 Simple Workouts Program</strong>
+      </p>
+    </header>
+  );
+
+  const footer = (
+    <footer>
+      <p>
+        Design & written by{" "}
+        <a
+          href="https://babucarr-badjie-portfolio.netlify.app/"
+          target="_blank"
+        >
+          Babucarr Badjie
+        </a>
+      </p>
+    </footer>
+  );
+  return (
+    <>
+      {header}
+      {children}
+      {footer}
+    </>
+  );
 }
